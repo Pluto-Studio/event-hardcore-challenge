9d87f99b4a6e9d8291fca4ba00b8acb5e047855c
nostalfinals <yuu8583@proton.me>
2025-01-19 15:59:49 +0800

    feat: more

--- feature-paper/src/main/kotlin/plutoproject/feature/paper/hardcoreChallenge/Challenge.kt
+++ feature-paper/src/main/kotlin/plutoproject/feature/paper/hardcoreChallenge/Challenge.kt
@@ -1,8 +1,15 @@
 package plutoproject.feature.paper.hardcoreChallenge
 
+import ink.pmc.advkt.component.text
+import ink.pmc.advkt.send
+import ink.pmc.advkt.showTitle
+import ink.pmc.advkt.title.mainTitle
+import org.bukkit.GameMode
 import org.bukkit.entity.Player
 import plutoproject.feature.paper.api.randomTeleport.RandomTeleportManager
 import plutoproject.feature.paper.api.randomTeleport.RandomTeleportOptions
+import plutoproject.framework.common.util.chat.palettes.mochaMaroon
+import plutoproject.framework.common.util.chat.palettes.mochaPink
 import plutoproject.framework.common.util.data.collection.mutableConcurrentSetOf
 import plutoproject.framework.paper.util.world.location.Position2D
 
@@ -24,19 +31,40 @@ val ChallengeRandomTeleportOptions = RandomTeleportOptions(
 )
 
 suspend fun startChallenge(player: Player) {
-    val model = UserRepository.findById(player.uniqueId) ?: UserModel(player.uniqueId)
-    if (model.isInChallenge) return
-    UserRepository.saveOrUpdate(model.copy(isInChallenge = true))
     inChallenge.add(player)
+    val model = UserRepository.findById(player.uniqueId) ?: UserModel(player.uniqueId)
+    if (!model.isInChallenge) {
+        UserRepository.saveOrUpdate(model.copy(isInChallenge = true))
+        onChallengeStart(player)
+    }
 }
 
 suspend fun stopChallenge(player: Player) {
-    val model = UserRepository.findById(player.uniqueId) ?: UserModel(player.uniqueId)
-    if (!model.isInChallenge) return
-    UserRepository.saveOrUpdate(model.copy(isInChallenge = false))
     inChallenge.remove(player)
+    val model = UserRepository.findById(player.uniqueId) ?: UserModel(player.uniqueId)
+    if (model.isInChallenge) {
+        UserRepository.saveOrUpdate(model.copy(isInChallenge = false))
+        onChallengeFailed(player)
+    }
+}
+
+suspend fun onChallengeStart(player: Player) {
+    player.gameMode = GameMode.SURVIVAL
+    performRandomTeleport(player)
+    player.send {
+        text("挑战已开始，利用规则存活下去吧！") with mochaPink
+    }
+}
+
+suspend fun onChallengeFailed(player: Player) {
+    player.gameMode = GameMode.SPECTATOR
+    player.showTitle {
+        mainTitle {
+            text("\uD83D\uDC80 挑战失败") with mochaMaroon
+        }
+    }
 }
 
 suspend fun performRandomTeleport(player: Player) {
-    RandomTeleportManager.launch(player, player.world, ChallengeRandomTeleportOptions)
+    RandomTeleportManager.launchSuspend(player, player.world, ChallengeRandomTeleportOptions)
 }
--- feature-paper/src/main/kotlin/plutoproject/feature/paper/hardcoreChallenge/listeners/PlayerListener.kt
+++ feature-paper/src/main/kotlin/plutoproject/feature/paper/hardcoreChallenge/listeners/PlayerListener.kt
@@ -1,26 +1,27 @@
 package plutoproject.feature.paper.hardcoreChallenge.listeners
 
-import ink.pmc.advkt.component.text
-import ink.pmc.advkt.send
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.PlayerDeathEvent
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import plutoproject.feature.paper.hardcoreChallenge.inChallenge
 import plutoproject.feature.paper.hardcoreChallenge.startChallenge
-import plutoproject.framework.common.util.chat.palettes.mochaPink
+import plutoproject.feature.paper.hardcoreChallenge.stopChallenge
 
 object PlayerListener : Listener {
     @EventHandler
     suspend fun PlayerJoinEvent.e() {
         startChallenge(player)
-        player.send {
-            text("挑战已开始，利用规则存活下去吧！") with mochaPink
-        }
     }
 
     @EventHandler
     fun PlayerQuitEvent.e() {
         inChallenge.remove(player)
+    }
+
+    @EventHandler
+    suspend fun PlayerDeathEvent.e() {
+        stopChallenge(player)
     }
 }
